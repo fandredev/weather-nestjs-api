9c5f1c5a6d332fd233345805e6be9d0084bebc7f
felipe <profissionalf.andre@gmail.com>
2024-11-23 01:30:52 -0300

    chore: add class validator and class transform to validate data from create/update a new user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,38 @@
-export class CreateUserDto {}
+import { IsString, IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+export class CreateUserDto {
+  @IsString({
+    message: 'Username must be a string',
+  })
+  @IsNotEmpty({
+    message: 'Username is required',
+  })
+  @MinLength(3, {
+    message: 'Username must be at least 3 characters long',
+  })
+  username: string;
+
+  @IsString({
+    message: 'Email must be a string',
+  })
+  @IsNotEmpty({
+    message: 'Email is required',
+  })
+  @IsEmail(
+    {},
+    {
+      message: 'Email must be a valid email address',
+    },
+  )
+  email: string;
+
+  @IsString({
+    message: 'Password must be a string',
+  })
+  @IsNotEmpty({
+    message: 'Password is required',
+  })
+  @MinLength(8, {
+    message: 'Password must be at least 8 characters long',
+  })
+  password: string;
+}
