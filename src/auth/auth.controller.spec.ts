4f9800727e9559867791f4d51c75d5e16784d360
felipe <profissionalf.andre@gmail.com>
2024-12-01 18:10:32 -0300

    tests: create tests for auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import generateRandomToken from './jwt/utils/generate-random-token';
+import { faker } from '@faker-js/faker/.';
 
-describe('AuthController', () => {
+describe(`${AuthController.name}`, () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            signIn: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  it('services and controller should be defined when module testing is created and compiled', () => {
     expect(controller).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe(`Tests for #${AuthService.prototype.signIn.name} method`, () => {
+    it(`should call #${AuthService.prototype.signIn.name} and return the access_token to user`, async () => {
+      const generatedRandomToken = generateRandomToken();
+
+      jest.spyOn(authService, 'signIn').mockResolvedValue({
+        access_token: generatedRandomToken,
+      });
+
+      const signIn = await controller.signIn({
+        id: 1,
+        password: faker.internet.password(),
+      });
+
+      expect(authService.signIn).toHaveBeenCalledWith(1, expect.any(String));
+      expect(signIn).toEqual({ access_token: generatedRandomToken });
+    });
   });
 });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import Users from 'src/users/interfaces/User';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Pick<Users, 'id' | 'password'>) {
+    return this.authService.signIn(signInDto.id, signInDto.password);
+  }
+}
